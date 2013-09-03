02f63021bcebc4bbf51ffe4184e705c72d0efff1
mshuffett <mshuffett@gmail.com>
2013-09-03 14:46:52 -0400

    Added oh-my-zsh

---
+++ oh-my-zsh/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+if command -v wget >/dev/null 2>&1; then
+  wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+else
+  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+fi
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,10 @@
+alias l=ls
+alias ll="ls -lh"
+alias dirs="dirs -v"
+alias sz="source ~/.zshrc"
+alias s="source ~/.zshrc"
+alias sa="source ~/.alias"
+alias ez="vim ~/.zshrc"
+alias ea="vim ~/.alias"
+alias shuffle="perl -MList::Util -e 'print List::Util::shuffle <>'"
 alias reload!='. ~/.zshrc'
