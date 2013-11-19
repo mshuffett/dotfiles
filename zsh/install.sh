3961e77188945ac1a0ea24357dd1a359b1ef6294
mshuffett <mshuffett@gmail.com>
2013-11-19 15:07:38 -0500

    Moved oh-my-zsh install code to zsh/install.sh so that zsh would always be installed first

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+sudo apt-get install zsh
+
 if command -v wget >/dev/null 2>&1; then
   wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 else
