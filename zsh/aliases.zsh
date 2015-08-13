14bd01f3914bb634803a0ed2bac1f46552bc70a8
mshuffett <mshuffett@gmail.com>
2015-08-12 23:28:25 -0700

    ZSH is only slow on RHEL5

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,7 +10,4 @@ alias shuffle="perl -MList::Util -e 'print List::Util::shuffle <>'"
 alias reload!='. ~/.zshrc'
 alias lh='ls -a | egrep "^\."' # ls dotfiles only
 
-# Vim is slow
-alias vim='vim -X'
-
 alias tmux='tmux -2'
