fb2779390230d9229a2418c06d571f00fa4b7ef0
mshuffett <mshuffett@gmail.com>
2015-12-06 13:19:55 -0800

    Refined aliases behavior more

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,19 +18,25 @@ alias lh='ls -a | egrep "^\."' # ls dotfiles only
 alias tmux='tmux -2'
 
 aa() {
+  local alias_name
+  local alias_command
   local new_alias
   [[ $1 == "alias" ]] && shift
   if (($# == 1)); then
-    new_alias=$1
+    # split argument based on first =
+    IFS='=' read -r alias_name alias_command <<< $1
   else
-    new_alias="$1=\"$@[2, -1]\""
+    alias_name=$1
+    alias_command="$@[2, -1]"
   fi
-  local alias_line="alias $new_alias"
+  alias "$alias_name=$alias_command"
+  local alias_line="alias $alias_name='$alias_command'"
   echo $alias_line >> $ALIASES_FILE
-  alias $new_alias
   echo "Added $alias_line to $ALIASES_FILE"
 }
 
 # User defined aliases (with aa)
 alias mmv='noglob zmv -W'
-alias 'gdiff=git diff'
+alias et="vim /home/michael/.tmux.conf"
+alias acs="apt-cache search"
+alias acs='apt-cache search'
