5c4e52daeb200f36ad020a619fc590f9672be7d3
mshuffett <mshuffett@gmail.com>
2015-12-12 01:21:43 -0800

    Added aag add-alias-global

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -17,23 +17,41 @@ alias lh='ls -a | egrep "^\."' # ls dotfiles only
 
 alias tmux='tmux -2'
 
-aa() {
-  local alias_name
-  local alias_command
-  local new_alias
-  [[ $1 == "alias" ]] && shift
-  if (($# == 1)); then
-    # split argument based on first =
-    IFS='=' read -r alias_name alias_command <<< $1
-  else
-    alias_name=$1
-    alias_command="$@[2, -1]"
-  fi
-  alias "$alias_name=$alias_command"
-  local alias_line="alias $alias_name='$alias_command'"
-  echo $alias_line >> $ALIASES_FILE
-  echo "Added $alias_line to $ALIASES_FILE"
+function {
+  local is_global=false
+  # Add alias
+  aa() {
+    local alias_name
+    local alias_command
+    local new_alias
+    local finished=false
+
+    [[ $1 == "alias" ]] && shift
+    if (($# == 1)); then
+      # split argument based on first =
+      IFS='=' read -r alias_name alias_command <<< $1
+    else
+      alias_name=$1
+      alias_command="$@[2, -1]"
+    fi
+
+    local cmd=alias
+    $is_global && $finished || cmd='alias -g'
+
+    eval $cmd "$alias_name=$alias_command"
+    local alias_line="$cmd $alias_name='$alias_command'"
+    echo $alias_line >> $ALIASES_FILE
+    echo "Added $alias_line to $ALIASES_FILE"
+    finished=true
+  }
+
+  # Add global alias
+  aag() {
+    is_global=true
+    aa $@
+  }
 }
+
 
 # User defined aliases (with aa)
 alias mmv='noglob zmv -W'
@@ -43,3 +61,6 @@ alias acs="apt-cache search"
 alias acs='apt-cache search'
 alias ac='apt-cache'
 alias agi='_ apt-get install'
+alias ags='acs'
+
+alias -g ws='/c/Users/Michael/Dropbox/ws'
