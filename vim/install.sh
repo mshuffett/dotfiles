078de04550ad74cb82a1bcaa9a016d0a1619e757
mshuffett <mshuffett@gmail.com>
2016-01-14 14:34:31 -0800

    Removed hardcoded apt-get install for vim

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install vim-gnome
+# sudo apt-get install vim-gnome
 
 # Install neobundle
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > /tmp/neo-install.sh
