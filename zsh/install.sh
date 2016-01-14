ad1c898431ae24bd75a48be9b7ee78900e75dec1
Michael Shuffett <shuffem@amazon.com>
2016-01-14 02:04:23 -0800

    Modified zsh install script to ignore macs and use antigen

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-sudo apt-get install zsh
-
-if command -v wget >/dev/null 2>&1; then
-  wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-else
-  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+if [ $(uname -s) != Darwin ]; then # we're not on a mac
+  sudo apt-get install zsh
 fi
