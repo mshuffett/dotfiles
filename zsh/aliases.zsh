59c75ef698f7078cbd60ba5304ac0f981e160c43
Michael Shuffett <michael@quantcollective.com>
2019-05-12 21:28:48 -0400

    Colorls config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -67,3 +67,6 @@ dlc() {
 alias db='docker build .'
 alias qn7='ssh qn7cimaterm01.cloud.corp.dig.com'
 alias pipenv='PIPENV_VENV_IN_PROJECT=1 pipenv'
+
+alias ls='/usr/local/bin/colorls -light -sort-dirs -report'
+alias lc='/usr/local/bin/colorls -tree -light'
